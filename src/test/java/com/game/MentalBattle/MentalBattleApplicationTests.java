7eb5e50e38a1d8358c6a822a9abeb2b90b2d1537
Roman <roman.suslov.d@gmail.com>
2022-07-26 09:13:21 +0300

    init

---
+++ src/test/java/com/game/MentalBattle/MentalBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.MentalBattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
