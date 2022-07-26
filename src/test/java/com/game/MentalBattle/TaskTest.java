76b8b71eab2d36952451fe207cbf310f756ecac5
Roman <roman.suslov.d@gmail.com>
2022-07-26 21:31:25 +0300

    add test of class Task

---
+++ src/test/java/com/game/MentalBattle/TaskTest.java
@@ -0,0 +1,23 @@
+package com.game.MentalBattle;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/test/taskTest.csv",delimiter = '|',lineSeparator = "&")
+    public void findOutputExampleTest(String condition,int answer,int random){
+        Task task = new Task();
+        Map<String,Integer> expected = new LinkedHashMap<>();
+        expected.put(condition,answer);
+        task.createExamples();
+        Map<String,Integer> actual = task.findOutputExample(random);
+        assertEquals(expected,actual);
+    }
+}
