a13bf66c9a260591af852d8a00090e8d27f20664
Roman <roman.suslov.d@gmail.com>
2022-07-26 21:26:47 +0300

    add config and model

---
+++ src/main/java/com/game/MentalBattle/Config.java
@@ -0,0 +1,8 @@
+package com.game.MentalBattle;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+public class Config {
+
+}
---
+++ src/main/java/com/game/MentalBattle/ModelGame.java
@@ -0,0 +1,7 @@
+package com.game.MentalBattle;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ModelGame {
+}
