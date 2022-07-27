d930b96249270dc9748fbda1095fd4793a875988
Roman <roman.suslov.d@gmail.com>
2022-07-27 19:40:35 +0300

    add source properties to config

--- src/main/java/com/game/MentalBattle/Config.java
+++ src/main/java/com/game/MentalBattle/Config.java
@@ -1,8 +1,10 @@
 package com.game.MentalBattle;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @ComponentScan
+@PropertySource("classpath:element.properties")
 public class Config {
 
 }
